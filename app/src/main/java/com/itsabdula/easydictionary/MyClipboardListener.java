bbd5e59f70c604ccdd50a0158ccdd86cbb534547
itsabdula <abdula2523@ajou.ac.kr>
2021-06-26 04:26:06 +0900

    최대한 얼리 리턴하게 변경, 코드 깔끔하게 정리

--- app/src/main/java/com/itsabdula/easydictionary/MyClipboardListener.java
+++ app/src/main/java/com/itsabdula/easydictionary/MyClipboardListener.java
@@ -5,15 +5,14 @@ import android.content.ClipDescription;
 import android.content.ClipboardManager;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 /**
  * Created by WooHyung_Choi on 2017-09-01.
  */
 
 public class MyClipboardListener implements ClipboardManager.OnPrimaryClipChangedListener {
-    private Context context;
-    String mPreviousText = "";
+    private final Context context;
+    String previous = "";
     ClipboardManager clipboard;
 
     public MyClipboardListener(Context context1) {
@@ -23,36 +22,22 @@ public class MyClipboardListener implements ClipboardManager.OnPrimaryClipChange
 
     @Override
     public void onPrimaryClipChanged() {
-        // Clipboard에 들어온 것이 MIMETYPE_TEXT_PLAIN과 MIMETYPE_TEXT_HTML이 아닐 때  && !clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_HTML)
-        if (!clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN) && !clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_HTML)) {
-            Log.e(this.getClass().getName(), "It's not MImeType_TEXT");
-            Log.d(this.getClass().getName(), clipboard.getPrimaryClipDescription().toString());
-            Log.d(this.getClass().getName(), clipboard.getPrimaryClip().getItemAt(0).getText().toString());
-
+        if (clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN) == false || clipboard.getPrimaryClipDescription().hasMimeType(ClipDescription.MIMETYPE_TEXT_HTML) == false) {
             return;
         }
 
-        // Clipboard에 들어온 것이 MIMETYPE_TEXT_PLAIN과 MIMETYPE_TEXT_HTML일 때
-        else {
-            try {
-                ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
-                if (item == null) throw new Throwable("NULL_CLIPDATA");
-                String clipDataWord = item.getText().toString();
+        ClipData.Item item = clipboard.getPrimaryClip().getItemAt(0);
+        if (item == null) return;
 
-                // 앞의 단어와 같은 경우 return;
-                if (mPreviousText.equals(clipDataWord)) throw new Throwable("Duplicated");
-                else {
-                    mPreviousText = clipDataWord;
+        String input = item.getText().toString();
+        input = input.replaceAll(" ", "");
+        if (previous.equals(input)) return;
 
-                    Intent intent = new Intent(context, GetFromNDic.class);
-                    intent.putExtra("Word", clipDataWord);
-                    context.startService(intent);
-                }
-            } catch (NullPointerException e) {
-                Log.e(this.getClass().getName(), "NullptrException");
-            } catch (Throwable t) {
-                Log.e(this.getClass().getName(), "Duplicated");
-            }
-        }
+        previous = input;
+
+        Intent intent = new Intent(context, GetFromNDic.class);
+        intent.putExtra("Word", input);
+
+        context.startService(intent);
     }
 }
